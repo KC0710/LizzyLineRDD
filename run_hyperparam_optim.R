43d0731a687e832fe894e69378465cf93c8e1988
Kai Cooper <kai.cooper19@imperial.ac.uk>
2023-01-31 17:01:15 +0000

    Run hyperparam optim

--- run_hyperparam_optim.R
+++ run_hyperparam_optim.R
@@ -9,6 +9,7 @@ library(lubridate)
 source(here("scripts", "bayes_optimise.R"))
 source(here("scripts", "hvblockedfolds.R"))
 source(here("scripts", "gbm.cverr.R"))
+source(here("scripts", "hyperparameter_optimisation.R"))
 load(here("data", "processed", "all_validated_sites.RData"))
 load(here("data", "processed", "meteo_full.RData"))
 
@@ -17,3 +18,9 @@ meteo_seasonality <- prepData(meteo_full, add=seasonality_vars) %>%
   select(-rain_12, -rain_06, -rain_06_modify,
          -wd_cos, -wd_sin, -wd_math,
          -dew_point, -ceil_hgt, -visibility)
+
+optim_runs_by_site <- lapply(all_validated_sites,
+                             FUN=run_hyperparameter_optimisation,
+                             meteo_seasonality)
+
+save(optim_runs_by_site, file=here("results", "optimal_hyperparams_by_site.RData"))
