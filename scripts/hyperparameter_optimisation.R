32a1e3d8caf24c01b5168d0a8fa17cd636c1f158
Kai Cooper <kai.cooper19@imperial.ac.uk>
2023-01-31 16:47:27 +0000

    Rename

--- scripts/hyperparameter_optimisation.R
+++ scripts/hyperparameter_optimisation.R
@@ -1,22 +1,3 @@
-library(here)
-library(deweather)
-library(mlrMBO)
-library(dplyr)
-library(smoof)
-library(tidyr)
-
-source(here("scripts", "bayes_optimise.R"))
-source(here("scripts", "hvblockedfolds.R"))
-source(here("scripts", "gbm.cverr.R"))
-load(here("data", "processed", "all_validated_sites.RData"))
-load(here("data", "processed", "meteo_full.RData"))
-
-seasonality_vars <- c("weekday", "jday", "trend")
-meteo_seasonality <- prepData(meteo_full, add=seasonality_vars) %>%
-  select(-rain_12, -rain_06, -rain_06_modify,
-         -wd_cos, -wd_sin, -wd_math,
-         -dew_point, -ceil_hgt, -visibility)
-
 gbm_inputs <- function(aq.dat, meteo.data){
   aq.dat_m <- aq.dat %>% rename(date = DateTime) %>%
     merge(meteo.data, by=c("date", "Year", "Month", "Day", "Hour")) %>%
@@ -34,8 +15,6 @@ gbm_inputs <- function(aq.dat, meteo.data){
 }
 
 run_hyperparameter_optimisation <- function(aq.dat, meteo.data){
-  
-  numpol <- length(unique(aq.dat$Pollutant))
   
   aq_by_pollutant <- split(aq.dat,
                            aq.dat$Pollutant)
